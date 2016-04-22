dbefbd2989803f91b4ae371ee0a1c6c0eac0cc24
Edward Lee <edwardlee0323@gmail.com>
2016-04-22 03:17:11 +0000

    updated

--- musician_class.py
+++ musician_class.py
@@ -1,54 +1,61 @@
 class Musician(object):
-    def __init__(self, sounds, name):
-        self.sounds = sounds
+    def __init__(self, name, sounds):
         self.name = name
+        self.sounds = sounds
     
     def solo(self, length):
         for i in range(length):
             print(self.sounds[i % len(self.sounds)])
-        print()
+            print('')
 
 
 class Bassist(Musician):
-    def __init__(self, name):
-        super(Bassist, self).__init__(name, ["Twang", "Thrumb", "Bling"])
+    def __init__(self, name, sounds):
+        super(Bassist, self, sounds).__init__(name, ["Twang", "Thrumb", "Bling"])
 
 class Guitarist(Musician):
     def __init__(self, name):
-        super(Guitarist, self).__init__(name, ["Boink", "Bow", "Boom"])
+        super(Guitarist, self, sounds).__init__(name, ["Boink", "Bow", "Boom"])
     
     def tune(self):
         print("Be with you in a moment")
         print("Twoning, sproing, splang")
 
 class Drummer(Musician):
-    def __init__(self, name):
-        super(Drummer, self).__init__(name, ["Thump", "Dun", "Spang"])
+    def __init__(self, name, sounds):
+        super(Drummer, self, sounds).__init__(name, ["Thump", "Dun", "Spang"], True)
     
     def countdown(self):
         for i in range(1,5):
             print(i)
 
 class Vocalist(Musician):
-    def __init__(self, name):
-        super(Vocalist, self).__init__(name, ["YEAHHH", "YOOOO", "WOOOO"])
+    def __init__(self, name, sounds):
+        super(Vocalist, self, sounds).__init__(name, ["YEAHHH", "YOOOO", "WOOOO"])
 
 class Band(object):
     def __init__(self):
         self.myBand = []
     def hire(self, member):
         self.myBand.append(member)
-        for member in self.myBand:
-            print(member.name)
+        print("Hired:", member.name)
+
     def fire(self, member):
         self.myBand.remove(member)
-        for member in self.myBand:
-            print(member.name)
-    def play(self, member):
-        if isinstance(self.myBand, Drummer):
-            member.countdown
-        for member in self.myBand:
-            member.solo
+        print("Fired:", member.name)
+
+    def play(self):
+        print("Current Band Members:", end = " ")
+        for members in self.myBand:
+            print(members.name, end = " ")
+        print("")
+        for position in self.myBand:
+            if isinstance(position, Drummer):
+                position.solo
+            else:
+                pass
+        for conduct in self.myBand:
+            conduct.solo
 
 amazing = Band()
 mark = Drummer("mark")
@@ -62,4 +69,4 @@ amazing.hire(mike)
 amazing.hire(kevin)
 amazing.hire(jack)
 amazing.fire(mark)
-amazing.play
+amazing.play()
